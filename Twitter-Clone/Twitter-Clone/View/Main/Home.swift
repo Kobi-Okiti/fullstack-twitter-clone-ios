421376be2592aa5aeb9664a4dc1206aa8a689ce5
kobi <kobiokiti@gmail.com>
2025-11-27 22:42:40 +0100

    developed the basic tab view and setup the file structure

--- Twitter-Clone/Twitter-Clone/View/Main/Home.swift
+++ Twitter-Clone/Twitter-Clone/View/Main/Home.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                TabView{
+                    Feed()
+                        .tabItem {
+                            Image("Home")
+                        }
+                    SearchView()
+                        .tabItem {
+                            Image("Search")
+                        }
+                    NotificationsView()
+                        .tabItem {
+                            Image("Notifications")
+                        }
+                    MessagesView()
+                        .tabItem {
+                            Image("Messages")
+                        }
+                }
+            }
+        }
     }
 }
 
