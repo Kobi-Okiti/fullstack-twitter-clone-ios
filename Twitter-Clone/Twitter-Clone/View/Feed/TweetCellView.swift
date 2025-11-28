75d9cc1aadfa18ba2d02af1ffd6377a882f61e84
kobi <kobiokiti@gmail.com>
2025-11-28 14:23:37 +0100

    created the tweet cell view

---
+++ Twitter-Clone/Twitter-Clone/View/Feed/TweetCellView.swift
@@ -0,0 +1,18 @@
+//
+//  TweetCellView.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 28/11/2025.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TweetCellView()
+}
