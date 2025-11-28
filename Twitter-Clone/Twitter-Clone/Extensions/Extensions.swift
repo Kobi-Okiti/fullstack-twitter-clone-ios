39b3bb744121786555ab12694140f3b154a89fad
kobi <kobiokiti@gmail.com>
2025-11-28 16:37:24 +0100

    created the files for our extensions and search user cell

---
+++ Twitter-Clone/Twitter-Clone/Extensions/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 28/11/2025.
+//
+
+import Foundation
---
+++ Twitter-Clone/Twitter-Clone/View/Search/SearchUserCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchUserCell.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 28/11/2025.
+//
+
+import SwiftUI
+
+struct SearchUserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchUserCell()
+}
