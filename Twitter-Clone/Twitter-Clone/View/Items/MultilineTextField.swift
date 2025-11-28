1c1005fec5979f9efbcadea9f029f444017858b2
kobi <kobiokiti@gmail.com>
2025-11-28 09:36:54 +0100

    Created a miltiline text field, developed the create tweet view and wired it to a sheet on click of the FAB

---
+++ Twitter-Clone/Twitter-Clone/View/Items/MultilineTextField.swift
@@ -0,0 +1,8 @@
+//
+//  MultilineTextField.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 28/11/2025.
+//
+
+import Foundation
---
+++ Twitter-Clone/Twitter-Clone/View/Post/CreateTweetView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTweetView.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 28/11/2025.
+//
+
+import SwiftUI
+
+struct CreateTweetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTweetView()
+}
