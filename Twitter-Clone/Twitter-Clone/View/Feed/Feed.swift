488555baeb7eaff2aa1ff5d539c887b53254183f
kobi <kobiokiti@gmail.com>
2025-11-28 14:24:05 +0100

    Created the feed view and populated it with the Tweet cell view

--- Twitter-Clone/Twitter-Clone/View/Feed/Feed.swift
+++ Twitter-Clone/Twitter-Clone/View/Feed/Feed.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct Feed: View {
     var body: some View {
-        Text("Feed")
+        ScrollView(.vertical, showsIndicators: false,
+                   content: {
+            VStack(spacing: 18) {
+                TweetCellView(tweet: "Hey Tim, are those regular glassses?", tweetImage: "post")
+                
+                Divider()
+                
+                ForEach(0..<20, id: \.self) { _ in
+                    TweetCellView(tweet: sampleText)
+                    Divider()
+                }
+            }.padding(.top)
+                .padding(.horizontal)
+                .zIndex(0)
+        })
     }
 }
 
--- Twitter-Clone/Twitter-Clone/View/Feed/TweetCellView.swift
+++ Twitter-Clone/Twitter-Clone/View/Feed/TweetCellView.swift
@@ -8,11 +8,80 @@
 import SwiftUI
 
 struct TweetCellView: View {
+    var tweet: String
+    var tweetImage: String?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0){
+            HStack(alignment: .top, spacing: 10, content: {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
+                VStack(alignment: .leading, spacing: 10,
+                       content: {
+                    (
+                        Text("Cam ")
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        +
+                        Text("@camoghenekobiruo")
+                            .foregroundColor(.gray)
+                    )
+                    
+                    Text(tweet)
+                        .frame(maxHeight: 100, alignment: .top)
+                    
+                    if let image = tweetImage {
+                        GeometryReader { proxy in
+                            Image(image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: proxy.frame(in: .global).width, height: 250)
+                                .cornerRadius(15)
+                        }
+                        .frame(height: 250)
+                        .padding(.bottom, 15)
+                    }
+                })
+            })
+//            Cell Bottom
+            HStack(spacing: 50,content: {
+                Button(action: {
+                    
+                }, label: {
+                    Image("Comments").resizable()
+                        .renderingMode(.template)
+                        .frame(width: 16, height: 16)
+                }).foregroundColor(.gray)
+                Button(action: {
+                    
+                }, label: {
+                    Image("Retweet").resizable()
+                        .renderingMode(.template)
+                        .frame(width: 18, height: 14)
+                }).foregroundColor(.gray)
+                Button(action: {
+                    
+                }, label: {
+                    Image("love").resizable()
+                        .renderingMode(.template)
+                        .frame(width: 18, height: 15)
+                }).foregroundColor(.gray)
+                Button(action: {
+                    
+                }, label: {
+                    Image("upload").resizable()
+                        .renderingMode(.template)
+                        .frame(width: 18, height: 15)
+                }).foregroundColor(.gray)
+            })
+        }
     }
 }
 
 #Preview {
-    TweetCellView()
+    TweetCellView(tweet: sampleText, tweetImage: "post")
 }
+
+var sampleText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex commodi fuga consequuntur maiores dolorum, quam, temporibus possimus laudantium odio ut sit animi, numquam assumenda! Assumenda eum asperiores enim quae."
