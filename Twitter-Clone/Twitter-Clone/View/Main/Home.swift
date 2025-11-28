52ee0c4f3efc94004d50c92787ddfc1818412c7a
kobi <kobiokiti@gmail.com>
2025-11-28 08:15:40 +0100

    updated the tabview with styling and logic fto show when a tab is active and added a floating action button

--- Twitter-Clone/Twitter-Clone/View/Main/Home.swift
+++ Twitter-Clone/Twitter-Clone/View/Main/Home.swift
@@ -8,27 +8,61 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var selectedIndex = 0
+    
     var body: some View {
         VStack {
             ZStack {
-                TabView{
+                TabView(selection: $selectedIndex){
                     Feed()
+                        .tag(0)
                         .tabItem {
                             Image("Home")
+                                .renderingMode(selectedIndex == 0 ? .template : .original)
+                                .foregroundColor(selectedIndex == 0 ? Color("bg") : .primary)
                         }
                     SearchView()
+                        .tag(1)
                         .tabItem {
                             Image("Search")
+                                .renderingMode(selectedIndex == 1 ? .template : .original)
+                                .foregroundColor(selectedIndex == 1 ? Color("bg") : .primary)
                         }
                     NotificationsView()
+                        .tag(2)
                         .tabItem {
                             Image("Notifications")
+                                .renderingMode(selectedIndex == 2 ? .template : .original)
+                                .foregroundColor(selectedIndex == 2 ? Color("bg") : .primary)
                         }
                     MessagesView()
+                        .tag(3)
                         .tabItem {
                             Image("Messages")
+                                .renderingMode(selectedIndex == 3 ? .template : .original)
+                                .foregroundColor(selectedIndex == 3 ? Color("bg") : .primary)
                         }
                 }
+                VStack {
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            
+                        }, label: {
+                            Image("Tweet").renderingMode(.template)
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .padding()
+                                .background(Color("bg"))
+                                .foregroundColor(.white)
+                                .clipShape(Circle())
+                        })
+                    }
+                    .padding()
+                }
+                .padding(.bottom, 65)
             }
         }
     }
