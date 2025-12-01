e01751dfbc3727b5890e9d1d635a42917c2e6331
kobi <kobiokiti@gmail.com>
2025-12-01 12:24:01 +0100

    Created the messages cell component and iterated it in the Messages View

--- Twitter-Clone/Twitter-Clone/View/Messages/MessageCell.swift
+++ Twitter-Clone/Twitter-Clone/View/Messages/MessageCell.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct MessageCell: View {
+    
+    @State var width = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: nil, content: {
+            Rectangle()
+                .frame(width: width, height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+            HStack{
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(30)
+                    .padding(.leading)
+                VStack(alignment: .leading, spacing: 0, content: {
+                    HStack{
+                        Text("Cem ")
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        
+                        Text("@oghenekobiruo")
+                            .foregroundColor(.gray)
+                        
+                        Spacer(minLength: 0)
+                        
+                        Text("6/20/21")
+                            .foregroundColor(.gray)
+                            .padding(.trailing)
+                    }
+                    Text("You: Hey! How is it going?")
+                        .foregroundColor(.gray)
+                    Spacer()
+                })
+            }
+            .padding(.top, 2)
+        })
+        .frame(width: width, height: 84)
     }
 }
 
