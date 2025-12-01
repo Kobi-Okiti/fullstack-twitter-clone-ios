3c6b12b3aeab9362ea03a8ab4dd2d1cbf79a28ca
kobi <kobiokiti@gmail.com>
2025-12-01 11:41:29 +0100

    created notification cell file

---
+++ Twitter-Clone/Twitter-Clone/View/Notifications/NotificationCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationCell.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 01/12/2025.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationCell()
+}
