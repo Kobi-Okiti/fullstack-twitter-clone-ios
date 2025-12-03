85d712dc03f530e9f9189444d2d6a5f31cfbb5cd
kobi <kobiokiti@gmail.com>
2025-12-03 20:26:01 +0100

    created the Banner and blur view in the user profile and also the Info text, still go tbugs to fix on it

--- Twitter-Clone/Twitter-Clone/View/Profile/BlurView.swift
+++ Twitter-Clone/Twitter-Clone/View/Profile/BlurView.swift
@@ -5,4 +5,15 @@
 //  Created by Oghenekobiruo Okiti on 03/12/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct BlurView:UIViewRepresentable {
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
--- Twitter-Clone/Twitter-Clone/View/Profile/UserProfile.swift
+++ Twitter-Clone/Twitter-Clone/View/Profile/UserProfile.swift
@@ -8,8 +8,135 @@
 import SwiftUI
 
 struct UserProfile: View {
+    
+    @State var offset: CGFloat = 0
+    @State var titleOffset: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: 100){
+                
+                GeometryReader { proxy -> AnyView in
+                    
+                    let minY = proxy.frame(in: .global).minY
+                    
+                    DispatchQueue.main.async{
+                        self.offset = minY
+                    }
+                    
+                    return AnyView(
+                        ZStack{
+                            Image("banner")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 180 + minY : 180, alignment: .center)
+                                .cornerRadius(0)
+                            
+                            BlurView()
+                                .opacity(blurViewOpacity())
+                            
+                            VStack(spacing: 5, content: {
+                                Text("Cem")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                Text("150 tweets")
+                                    .foregroundColor(.white)
+                            })
+                            .offset(y: 120)
+                            .offset(y: titleOffset > 100 ? 0 : -getTitleOffset())
+                            .opacity(titleOffset < 100 ? 1 : 0)
+                        }
+                            .clipped()
+                            .frame(height: minY > 0 ? 180 + minY : nil)
+                            .offset(y: minY > 0 ? -minY : -minY < 80 ? 0 : -minY - 80)
+                    )
+                }
+                .frame(height: 100)
+                .zIndex(1)
+                
+                VStack{
+                    HStack{
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white)
+                            .offset(y: offset < 0 ? getOffset() - 20 : -20)
+                            .scaleEffect(getScale())
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Edit Profile")
+                                .foregroundColor(.blue)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(Capsule().stroke(Color.blue, lineWidth: 1.5))
+                        })
+                    }
+                    .padding(.top, -25)
+                    .padding(.bottom, -10)
+                    
+                    VStack(alignment: .leading, spacing: 8, content: {
+                        Text("Cem")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        
+                        Text("@oghenekobiruo")
+                            .foregroundColor(.gray)
+                        
+                        Text("Mehn we all do not know what we're doing, and we never will")
+                        
+                        HStack(spacing: 5, content: {
+                            Text("13")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                            
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                            
+                            Text("680")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                                .padding(.leading, 10)
+                            
+                            Text("Following")
+                                .foregroundColor(.gray)
+                        })
+                    })
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    func blurViewOpacity() -> Double {
+        let progress = -(offset + 80) / 150
+        return Double(-offset > 80 ? progress : 0)
+    }
+    
+    func getTitleOffset() -> CGFloat {
+        let progress = 20 / titleOffset
+        let offset = 60 * (progress > 0 && progress <= 1 ? progress : 1)
+        return offset
+    }
+    
+    func getOffset() -> CGFloat {
+        let progress = (-offset / 80) * 20
+        return progress <= 20 ? progress : 20
+    }
+    
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1)
+        return scale < 1 ? scale : 1
     }
 }
 
