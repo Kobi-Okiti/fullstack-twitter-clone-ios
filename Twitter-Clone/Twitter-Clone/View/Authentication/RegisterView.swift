a4b13b38f116cf51337c831bd37c8404e0aa6bf7
kobi <kobiokiti@gmail.com>
2025-12-05 18:05:45 +0100

    created the files for my text fields and the register view

---
+++ Twitter-Clone/Twitter-Clone/View/Authentication/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 05/12/2025.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
---
+++ Twitter-Clone/Twitter-Clone/View/Items/SecureAuthTextField.swift
@@ -0,0 +1,18 @@
+//
+//  SecureAuthTextField.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 05/12/2025.
+//
+
+import SwiftUI
+
+struct SecureAuthTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecureAuthTextField()
+}
