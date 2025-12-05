695443142bd7866dd0042a5361c3134799dfa0b0
kobi <kobiokiti@gmail.com>
2025-12-05 18:06:13 +0100

    Created the Custom auth text fields, the Secure auth text fileds and used them i the register view i developed

--- Twitter-Clone/Twitter-Clone/View/Authentication/RegisterView.swift
+++ Twitter-Clone/Twitter-Clone/View/Authentication/RegisterView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ZStack{
+                HStack{
+                    Button {
+                        
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 35)
+            
+            VStack(alignment: .leading, spacing: nil, content: {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+                
+            })
+            Spacer(minLength: 0)
+            VStack{
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color(red: 29/255, green: 161/255, blue:242/255))
+                            .overlay(
+                                Text("Next")
+                                    .foregroundColor(.white)
+                            )
+                    }
+
+                }
+                .padding(.trailing, 24)
+            }
+        }
     }
 }
 
