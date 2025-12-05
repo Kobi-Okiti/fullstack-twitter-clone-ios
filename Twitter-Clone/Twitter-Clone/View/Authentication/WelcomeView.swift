476b53d1d8eb731ad8035929d5f93ce7c06e2599
kobi <kobiokiti@gmail.com>
2025-12-05 17:14:55 +0100

    created file for Welcome View

---
+++ Twitter-Clone/Twitter-Clone/View/Authentication/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 05/12/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
