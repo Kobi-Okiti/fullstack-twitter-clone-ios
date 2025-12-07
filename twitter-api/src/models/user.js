9460d2487e863c162e87ed5017ea2062d91ba9b3
kobi <kobiokiti@gmail.com>
2025-12-07 22:39:38 +0100

    installed bcryptjs package used bcrypt to hash user password.

--- twitter-api/src/models/user.js
+++ twitter-api/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -61,6 +62,7 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// To delete password prior to GET
 userSchema.methods.toJSON = function (){
     const user = this
     const userObject = user.toObject()
@@ -69,6 +71,16 @@ userSchema.methods.toJSON = function (){
 
     return userObject
 }
+
+// To Hash the password
+userSchema.pre('save', async function() {
+    const user = this
+
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+})
 
 const User = mongoose.model('User', userSchema)
 
