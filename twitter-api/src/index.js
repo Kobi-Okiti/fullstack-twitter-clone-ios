af76b60468ddeae29841faebda758a11d5fe90e0
kobi <kobiokiti@gmail.com>
2025-12-07 21:47:18 +0100

    made route to create users

--- twitter-api/src/index.js
+++ twitter-api/src/index.js
@@ -3,7 +3,12 @@ require('./db/mongoose')
 
 const app = express()
 
+const userRouter = require('./router/user')
+
 const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(userRouter)
 
 app.listen(port, ()=> {
     console.log("server is up on the port " + port)
