7ca1d68ba29e34d2d3887d8c0288a55a5186b4fa
kobi <kobiokiti@gmail.com>
2025-12-08 11:04:10 +0100

    created route for deleting a user

--- twitter-api/src/router/user.js
+++ twitter-api/src/router/user.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 
 // Original Router
 const router = new express.Router();
@@ -7,37 +7,53 @@ const router = new express.Router();
 // Endpoints
 
 // Create a new user
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    }
-    catch(e){
-        res.status(400).send(e)
-    }
-})
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 // Fetch the users
-router.get('/users',  async (req, res) =>{
-    try{
-        const users = await User.find({})
-        res.send(users)
-    }
-    catch(e){
-        res.status(500).send(e)
-    }
-})
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 // Login User Router
-router.post('/users/login', async (req, res) => {
-    try{
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({user, token})
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// Delete User Router
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(400).send();
     }
-    catch(e){
-        res.status(500).send()
-    }
-})
-module.exports = router
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
