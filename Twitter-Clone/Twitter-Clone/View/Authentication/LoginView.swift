e9674e727922d8d1fc4dc9ad89393f718d686bac
kobi <kobiokiti@gmail.com>
2025-12-10 18:52:05 +0100

    Created the login function for the login services

--- Twitter-Clone/Twitter-Clone/View/Authentication/LoginView.swift
+++ Twitter-Clone/Twitter-Clone/View/Authentication/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @StateObject var viewModel = AuthViewModel()
+    
     @State var email: String = ""
     @State var password = ""
     @State var emailDone = false
@@ -96,7 +98,7 @@ struct LoginView: View {
                 
                 VStack{
                     Button (action:{
-                        self.emailDone.toggle()
+                        self.viewModel.login(email: email, password: password)
                     }, label: {
                         Capsule()
                             .frame(width: 360, height: 40, alignment: .center)
--- Twitter-Clone/Twitter-Clone/ViewModel/AuthViewModel.swift
+++ Twitter-Clone/Twitter-Clone/ViewModel/AuthViewModel.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 class AuthViewModel: ObservableObject {
-    func login(){
+    
+    @Published var isAuthenticated: Bool = false
+    @Published var currentUser: User?
+    
+    func login(email: String, password: String){
         
+        let defaults = UserDefaults.standard
+        
+        AuthServices.login(email: email, password: password) { result in
+            switch result {
+            case .success(let data):
+                guard let user = try? JSONDecoder().decode(ApiResponse.self, from: data as! Data) else{return}
+                DispatchQueue.main.async{
+                    defaults.set(user.token, forKey: "jsonwebtoken")
+                    defaults.set(user.user.id, forKey: "userid")
+                    self.isAuthenticated = true
+                    self.currentUser = user.user
+                    print("Logged in!")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
-    func register(name: String, username: String, email: String, password: String, ){
+    func register(name: String, username: String, email: String, password: String){
         AuthServices.register(email: email, username: username, password: password, name: name) { result in
             switch result{
             case .success(let data):
