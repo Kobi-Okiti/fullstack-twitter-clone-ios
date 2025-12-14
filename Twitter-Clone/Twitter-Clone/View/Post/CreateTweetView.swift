051d1d90fe6e01613d4a8f2a6346598b8de1ebb0
kobi <kobiokiti@gmail.com>
2025-12-14 22:49:14 +0100

    Added the image picker in the create tweet view

--- Twitter-Clone/Twitter-Clone/View/Post/CreateTweetView.swift
+++ Twitter-Clone/Twitter-Clone/View/Post/CreateTweetView.swift
@@ -11,6 +11,10 @@ struct CreateTweetView: View {
     
     @Binding var show : Bool
     @State var text = ""
+    @State var imagePickerPresented = false
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var width = UIScreen.main.bounds.width
     
     @ObservedObject var viewModel = CreateTweetViewModel()
     
@@ -39,6 +43,47 @@ struct CreateTweetView: View {
                 .clipShape(Capsule())
             }
             MultilineTextField(text: $text)
+            
+            if postImage == nil {
+                Button {
+                    imagePickerPresented.toggle()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 70)
+                        .clipped()
+                        .padding(.top)
+                        .foregroundColor(.black)
+                }.sheet(isPresented: $imagePickerPresented) {
+                    loadImage()
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+            }
+            else if let image = postImage {
+                VStack{
+                    HStack(alignment: .top) {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.horizontal)
+                            .frame(width: width * 0.9)
+                            .cornerRadius(16)
+                            .clipped()
+                    }
+                    Spacer()
+                }
+            }
+
+            
         }.padding()
+    }
+}
+
+extension CreateTweetView {
+    func loadImage(){
+        guard let selectedImage = selectedImage else {return}
+        postImage = Image(uiImage: selectedImage)
     }
 }
