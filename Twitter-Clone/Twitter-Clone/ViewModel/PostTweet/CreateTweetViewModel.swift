270eeb0491fa95819803fb26989e1cd766c25ad3
kobi <kobiokiti@gmail.com>
2025-12-14 23:37:03 +0100

    Implemented uploading Images if used in tweet

--- Twitter-Clone/Twitter-Clone/ViewModel/PostTweet/CreateTweetViewModel.swift
+++ Twitter-Clone/Twitter-Clone/ViewModel/PostTweet/CreateTweetViewModel.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 class CreateTweetViewModel: ObservableObject {
-    func uploadPost(text: String){
+    func uploadPost(text: String, image: UIImage?){
         guard let user = AuthViewModel.shared.currentUser else {return}
         
         RequestServices.requestDomain = "http://localhost:3000/tweets"
         
         RequestServices.postTweet(text: text, user: user.name, username: user.username, userId: user.id) { result in
-            print(result)
+            if let image = image{
+                if let id = result?["_id"]!{
+                    ImageUploader.uploadImage(paramName: "upload", fileName: "image2", image: image, urlPath: "/uploadTweetImage/\(id)")
+                }
+            }
         }
     }
 }
