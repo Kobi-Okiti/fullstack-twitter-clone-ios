435872d8be38b796c93cff4ea0866326224c5322
kobi <kobiokiti@gmail.com>
2025-12-16 09:46:41 +0100

    Created the Custom Text and Bio Fields and the edit profile view.

---
+++ Twitter-Clone/Twitter-Clone/View/Items/CustomProfileBioTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomProfileBioTextField.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 16/12/2025.
+//
+
+import SwiftUI
+
+struct CustomProfileBioTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomProfileBioTextField()
+}
---
+++ Twitter-Clone/Twitter-Clone/View/Items/CustomProfileTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomProfileTextField.swift
+//  Twitter-Clone
+//
+//  Created by Oghenekobiruo Okiti on 16/12/2025.
+//
+
+import SwiftUI
+
+struct CustomProfileTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomProfileTextField()
+}
