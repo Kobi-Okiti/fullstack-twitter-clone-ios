977484fc6bfa4aa0225dd296dc42b6cace96c035
kobi <kobiokiti@gmail.com>
2025-12-16 17:30:00 +0100

    Implemented the save EditProfileView

--- Twitter-Clone/Twitter-Clone/ViewModel/Profile/EditProfileViewModel.swift
+++ Twitter-Clone/Twitter-Clone/ViewModel/Profile/EditProfileViewModel.swift
@@ -5,4 +5,24 @@
 //  Created by Oghenekobiruo Okiti on 16/12/2025.
 //
 
-import Foundation
+import SwiftUI
+
+class EditProfileViewModel: ObservableObject{
+    var user: User
+    
+    init(user: User){
+        self.user = user
+    }
+    
+    func save(name: String?, bio: String?, website: String?, location: String?){
+        guard let userNewName = name else {return}
+        guard let userNewBio = bio else {return}
+        guard let userNewWebsite = website else {return}
+        guard let userNewLocation = location else {return}
+        
+        self.user.name = userNewName
+        self.user.bio = userNewBio
+        self.user.website = userNewWebsite
+        self.user.location = userNewLocation
+    }
+}
